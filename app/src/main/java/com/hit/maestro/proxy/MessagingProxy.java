d6136bd8a445c11b181eb55d29e50cba3a76d7d3
SaarSkittel <saar23042gmail.com>
2021-09-19 11:30:12 +0300

    messaging service+messaging proxy

---
+++ app/src/main/java/com/hit/maestro/proxy/MessagingProxy.java
@@ -0,0 +1,64 @@
+package com.hit.maestro.proxy;
+
+import android.content.Context;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public  class MessagingProxy {
+    final String APP_KEY="AAAAiElpjxU:APA91bG3BKh_XjVfcxfGQS8BNHPsH00gveXrxPrrBSn23AuRyw0l-dNqL0wkuW8gU3N-A5ro-9Reanf2pED2JQYlnWl9fXjfC6yT6GJKeXKFa-ZiQYFYd34uan6do614a6yUf-nGJ2mi";
+    public void SendMessage(String to, String message, Context context){
+        JSONObject jsonObject= new JSONObject();
+
+        try {
+            jsonObject.put("to","/topics/"+to);
+            jsonObject.put("data",new JSONObject().put("message",message));
+            String url="https://fcm.googleapis.com/fcm/send";
+            RequestQueue queue= Volley.newRequestQueue(context);
+            StringRequest request=new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+
+
+                    //Send message when something goes wrong
+
+                }
+            }){
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    Map<String,String> headers=new HashMap<>();
+                    headers.put("Content-Type","application/json");
+                    headers.put("Authorization","key="+APP_KEY);
+                    return headers;
+                }
+
+                @Override
+                public byte[] getBody() throws AuthFailureError {
+                    return jsonObject.toString().getBytes();
+                }
+            };
+            queue.add(request);
+            queue.start();
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
---
+++ app/src/main/java/com/hit/maestro/services/MessagingService.java
@@ -0,0 +1,67 @@
+package com.hit.maestro.services;
+
+import android.content.Intent;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MessagingService extends FirebaseMessagingService {
+    final String TAG="MessagingService";
+    private FirebaseDatabase database;
+    private DatabaseReference user;
+
+
+
+    @Override
+    public void onNewToken(@NonNull String s) {
+        super.onNewToken(s);
+        Log.e(TAG,"service is up");
+        database= FirebaseDatabase.getInstance();
+        user=database.getReference().child("users");
+        user.child(s).setValue(s).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+
+                    Log.e(TAG,"success!");
+                }
+                else {
+                    Log.e(TAG,task.getException().getMessage());
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        // ...
+
+        // TODO(developer): Handle FCM messages here.
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {//from: /topics/A ==>/topics/UID, /topics/guitar
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+            Intent intent= new Intent("message_received");
+            intent.putExtra("message",remoteMessage.getSenderId()+ ":" + remoteMessage.getData().get("message"));
+            LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        }
+    }
+}

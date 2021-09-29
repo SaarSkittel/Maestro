cdeac473b3e27eece24917c63669753423232029
IdoAmira <68790040+IdoAmira@users.noreply.github.com>
2021-09-29 13:01:39 +0300

    321

--- app/src/main/java/com/hit/maestro/fragments/PictureFragment.java
+++ app/src/main/java/com/hit/maestro/fragments/PictureFragment.java
@@ -6,18 +6,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -27,138 +24,63 @@ import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
-
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.Nullable;
 import androidx.core.content.FileProvider;
 import androidx.fragment.app.DialogFragment;
-
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.request.RequestOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.imageview.ShapeableImageView;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.UserProfileChangeRequest;
 import com.hit.maestro.R;
-import com.hit.maestro.User;
 
 import java.io.File;
+import java.io.UnsupportedEncodingException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class RegisterFragment extends DialogFragment {
-
-    interface OnCompletedFragmentListener{
-        void onCompleted();
-        void onSignInFromRegisterFragment();
-    }
-
-    User user;
-    final String PICTURE_TAG="6";
-    OnCompletedFragmentListener callBack;
-    ShapeableImageView picture;
-    //ImageView picture;
+public class PictureFragment extends DialogFragment {
+    FloatingActionButton camera;
+    FloatingActionButton album;
+    Button save;
+    ImageView picture;
+    String source;
     File file;
-    Uri pic=Uri.parse("android.resource://com.hit.maestro/drawable/default_profile_picture");
+    Uri pic;
+    TextView title;
     boolean exit=false;
     final int write_permission_request_camera = 1;
     final int write_permission_request_album = 2;
-    Button camera, album;
     ActivityResultLauncher<Intent> takePictureActivityResultLauncher;
     ActivityResultLauncher<Intent> picFromAlbumActivityResultLauncher;
+    RegisterFragment registerFragment;
     View view;
 
-    public RegisterFragment(OnCompletedFragmentListener callBack) {
-        this.callBack = callBack;
-    }
-
+    @Nullable
     @Override
-    public void onAttach(@NonNull Context context) {
-        super.onAttach(context);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.picture_fragment,container,false);
+        title=view.findViewById(R.id.change_pic_tv);
 
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.register_fragment,container,false);
-        EditText fullnameET = view.findViewById(R.id.fullname_input);
-        EditText emailET = view.findViewById(R.id.email_input);
-        EditText passwordET = view.findViewById(R.id.password_input);
-        EditText passwordConfET = view.findViewById(R.id.confirmPassword_input);
-        TextView note = view.findViewById(R.id.note);
-
-        user=User.getInstance();
-        Button registerBtn = view.findViewById(R.id.register_btn);
-        registerBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(fullnameET.getText().toString().isEmpty()||emailET.getText().toString().isEmpty()||passwordET.getText().toString().isEmpty()||passwordConfET.getText().toString().isEmpty()){
-                    note.setText("Please fill all fields");
-                }
-                else if(!passwordET.getText().toString().equals(passwordConfET.getText().toString())){
-                    note.setText("Make sure the password is correct");
-                }
-                /*else if(!exit){
-                    note.setText("Please select a profile picture");
-                }*/
-                else{
-                   /* user.getFirebaseAuth().createUserWithEmailAndPassword(emailET.getText().toString(),passwordET.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if(task.isSuccessful()){
-                                user.getFirebaseUser().updateProfile(new UserProfileChangeRequest.Builder().setDisplayName(fullname).build());
-                                note.setText("Sign up successful");
-                            }
-                            else {
-                                note.setText(task.getException().getMessage());
-                            }
-                        }
-                    });*/
-                    note.setText("Sign up successful, please wait");
-                    user.CreateUser(fullnameET.getText().toString(),emailET.getText().toString(),passwordET.getText().toString(),pic.toString());
-                    //user.setUserData();
-                    callBack.onCompleted();
-                    RegisterFragment.this.dismiss();
-                }
-            }
-        });
-
-        TextView signInBtn = view.findViewById(R.id.sign_in_btn);
-        signInBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                callBack.onSignInFromRegisterFragment();
-            }
-        });
-
-        Button selectPic = view.findViewById(R.id.add_pic);
-        selectPic.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                PictureFragment pictureFragment=new PictureFragment(RegisterFragment.this);
-                pictureFragment.show(getChildFragmentManager(),PICTURE_TAG);
-            }
-        });
-
-        //pic = Uri.parse(sp.getString("path_pic",null));
-        picture = view.findViewById(R.id.user_image);
+        camera=view.findViewById(R.id.camera_btn);
+        album=view.findViewById(R.id.album_btn);
+        picture=view.findViewById(R.id.song_iv);
+        save=view.findViewById(R.id.save_pic_btn);
+        //pic =Uri.parse(requireArguments().getString("image"));
+        pic = registerFragment.getPic();
         Glide.with(this)
                 .load(pic)
                 .apply(RequestOptions.skipMemoryCacheOf(true))
                 .apply(RequestOptions.diskCacheStrategyOf(DiskCacheStrategy.NONE))
                 .into(picture);
 
-        /*777file = new File(getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES),new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date())+".jpg");
+        file = new File(getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES),new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date())+".jpg");
         takePictureActivityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
             @Override
             public void onActivityResult(ActivityResult result) {
                 if (result.getResultCode() == Activity.RESULT_OK) {
                     exit=true;
-                    //Glide.with(RegisterFragment.this).load(pic).into(picture);
+                    Glide.with(PictureFragment.this).load(pic).into(picture);
                 }
             }
         });
@@ -167,7 +89,7 @@ public class RegisterFragment extends DialogFragment {
             @Override
             public void onActivityResult(ActivityResult result) {
                 if (result.getResultCode() == Activity.RESULT_OK) {
-                    //Glide.with(RegisterFragment.this).load(result.getData().getData()).into(picture);
+                    Glide.with(PictureFragment.this).load(result.getData().getData()).into(picture);
                     pic=result.getData().getData();
                     view.getContext().getContentResolver().takePersistableUriPermission(pic
                             , result.getData().getFlags()
@@ -180,8 +102,14 @@ public class RegisterFragment extends DialogFragment {
             }
         });
 
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerFragment.setPic(pic);
+                PictureFragment.this.dismiss();
+            }
+        });
 
-        camera = view.findViewById(R.id.camera);
         camera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -200,7 +128,6 @@ public class RegisterFragment extends DialogFragment {
             }
         });
 
-        album = view.findViewById(R.id.gallery);
         album.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -215,27 +142,14 @@ public class RegisterFragment extends DialogFragment {
                     picFromMemory();
                 }
             }
-        });777*/
+        });
         return view;
     }
 
-    public void setPic(Uri pic) {
-        this.pic = pic;
-        Glide.with(this)
-                .load(pic)
-                .apply(RequestOptions.skipMemoryCacheOf(true))
-                .apply(RequestOptions.diskCacheStrategyOf(DiskCacheStrategy.NONE))
-                .into(picture);
-    }
-
-    public Uri getPic() {
-        return pic;
-    }
-
-    /*private void picFromMemory(){
+    private void picFromMemory(){
         exit=false;
-        Intent intent= new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.INTERNAL_CONTENT_URI);
-        //Intent intent= new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        //Intent intent= new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        Intent intent= new Intent(Intent.ACTION_OPEN_DOCUMENT, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         intent.setType("image/*");
         intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
         intent.addFlags(Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION);
@@ -264,5 +178,9 @@ public class RegisterFragment extends DialogFragment {
                 Toast.makeText(view.getContext(),"Can't work without permissions go to setting to grant access.",Toast.LENGTH_LONG).show();
             }
         }
-    }*/
+    }
+
+    public PictureFragment(RegisterFragment registerFragment) {
+        this.registerFragment = registerFragment;
+    }
 }
